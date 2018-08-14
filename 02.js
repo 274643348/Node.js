0348ccc245771d726b6dfdc24593067ef9964b30
XiaoYan <liujingyan@congmingpai.com>
2018-08-14 15:31:09 +0800

    export和require是什么?

---
+++ 02.js
@@ -0,0 +1,19 @@
+
+
+
+// console.log(global);
+// console.log(global.exports); //undefined
+// console.log(global.require);//undefined
+// console.log(arguments);
+
+// console.log(arguments.callee);//Function
+console.log(arguments.callee +"");
+
+// function (exports, require, module, __filename, __dirname) { // console.log(global);
+//     // console.log(global.exports); //undefined
+//     // console.log(global.require);//undefined
+//     // console.log(arguments);
+    
+//     // console.log(arguments.callee);//Function
+//     console.log(arguments.callee +"");
+//     }
