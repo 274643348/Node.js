842557b47f8deb265fddf8a30d1c8ed1e02b37b1
XiaoYan <liujingyan@congmingpai.com>
2018-08-18 16:17:54 +0800

    express搭建静态资源库-虚拟静态资源库路径

--- Express/74(搭建静态资源裤).js
+++ Express/74(搭建静态资源裤).js
@@ -1,9 +1,13 @@
 let express = require("express");
 let path = require("path");
 let app =express();
-app.use(express.static(path.normalize(__dirname + "/web-mobile")));
 
-app.get("/a",(req,res)=>{
+//所有文件的路径都是相对于存放目录的，因此，存放静态文件的目录名不会出现在 URL 中。
+
+
+app.use("/game",express.static(path.normalize(__dirname + "/web-mobile")));
+app.use("/game2",express.static(path.normalize(__dirname + "/web-mobile1")));
+app.get("/",(req,res)=>{
     res.end("hello world");
 });
 
